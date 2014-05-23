2ca1a7d5e45679a97cd84652c49c142f7a9aaa1d
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-05-23 18:39:34 +0200

    [ Update ] Now fully works

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,37 @@
 (function() {
+    'use strict';
     var app = angular.module('contacts', []);
+
+    /**
+    * TestController
+    * @description Check if localStorage is available
+    **/
+    app.controller('TestController', function() {
+        this.supportStorage = function() {
+            try {
+                return 'localStorage' in window && window['localStorage'] !== null;
+            } catch (e) {
+                return false;
+            }
+        };
+    });
 
     app.controller('ContactsController', ['$http', '$scope', '$window', function($http, $scope, $window) {
         // Our contacts will be stored here, Init with default value :D
         $scope.contacts = [];
         // Init new contact
         $scope.contact = {};
+        if($window.localStorage['contacts']) {
+            // Resuming your tasks regitered on $window.localstorage
+            var cs = JSON.parse($window.localStorage['contacts']);
+            // Updating global tasks variable
+            $scope.contacts.push(cs);
+            console.log($scope.contacts);
+            $scope.contacts = $scope.contacts[0];
+        } else {
+            // Init new $window.localstorage Object and tasks global var
+            $window.localStorage['contacts'] = [];
+        }
         // Determine how many contacts the users have
         $scope.count = $scope.contacts.length;
         // Determine the user's action
@@ -14,6 +40,11 @@
             if($scope.action == 'cancel') {
                 $scope.contact  = {};
             }
+            if($scope.action == 'delete') {
+                $window.localStorage.removeItem('contacts')
+                $scope.contacts = [];
+            }
+            $scope.count = $scope.contacts.length;
         };
         // Return the user's action
         $scope.getAction = function(action) {
@@ -42,6 +73,8 @@
         // Add new contact
         $scope.saveContact = function(contact) {
             $scope.contacts.push(contact);
+            $window.localStorage.setItem('contacts', JSON.stringify($scope.contacts));
+            $scope.count = $scope.contacts.length;
             $scope.contact = {};
         };
     }]);
